f24dfd5bbd1269fd03cfcde539f0ddc86c64086a
Venkata Deekshith Reddy, Chilakala <deekshithreddychilakala@gmail.com>
2020-12-01 23:53:45 +0530

    added variables

--- Fundamentals-Part1/script.js
+++ Fundamentals-Part1/script.js
@@ -1,7 +1,22 @@
 let js = 'amazing';
 
-if (js === 'amazing') {
-    alert('JavaScript is FUN!');
-}
-
 console.log(40 + 8 + 23 - 10)
+
+console.log('Jonas');  // here jonas is a value
+
+let firstName = 'Matilda';
+console.log('firstName: ', firstName);
+console.log('firstName: ', firstName);
+console.log('firstName: ', firstName);
+
+let jonas_matilda = "JM";
+let $function = 27;
+
+let person = 'jonas';
+let PI = 3.1415;
+
+let myFirstJob = 'programmer';
+let myCurrentJob = 'Developer';
+
+let job1 = 'programmer';
+let job2 = 'teacher';
