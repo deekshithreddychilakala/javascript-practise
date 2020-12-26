f12fb6c48fc6ee7c20489808645e8f170136f9d3
Venkata Deekshith Reddy, Chilakala <deekshithreddychilakala@gmail.com>
2020-12-26 11:17:31 +0530

    Fundamentals part 1 assignment 1

---
+++ Fundamentals-Part1/assignments.js
@@ -0,0 +1,26 @@
+// Fundamentals Part 1
+// LECTURE: Values and Variables
+// 1. Declare variables called 'country', 'continent' and 'population' and
+// assign their values according to your own country (population in millions)
+// 2. Log their values to the console
+console.log('====================================================================================================================================');
+console.log('Assignments');
+console.log('====================================================================================================================================');
+
+const country = 'India';
+const continent = 'Asia';
+const population = '135.26 Cr';
+
+console.log('Country: ', country);
+console.log('Continent: ', continent);
+console.log('Population: ', population);
+
+
+
+
+
+
+
+
+
+console.log('====================================================================================================================================');
